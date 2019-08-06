cca931c0422c5ca80f191118768c5788055cfd0f
noelru94 <noelru94@gmail.com>
2019-08-06 16:31:01 -0400

    function concertThis(), when envoked using axios will make a GET request to the bands in town api and display event information pertaining to the artist the user chose to search

--- liri.js
+++ liri.js
@@ -27,3 +27,41 @@ switch(input){
     break;
 
 }
+
+function concertThis(){
+
+    var artist = process.argv[3];
+
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(queryURL) 
+        .then(function(response){
+            console.log(response.data[0]);
+            var eventsArray = response.data;
+
+            for(let i = 0; i < eventsArray.length; i++){
+               
+               var venueName = eventsArray[i].venue.name;
+               var venueLocation = `${eventsArray[i].venue.city}, ${eventsArray[i].venue.country}`;
+               var eventDate = moment(eventsArray[i].datetime);
+
+               console.log(`Artist: ${artist}`);
+               console.log(`Venue: ${venueName}`);
+               console.log(`Location: ${venueLocation}`) 
+               console.log(`Date: ${eventDate}`);   
+               console.log('----------------------------');
+            }
+        })
+        .catch(function(error){
+            if(error.response) {
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                console.log(error.request);
+            }else {
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
+        })
+}
