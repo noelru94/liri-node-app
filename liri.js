c0d00ce1591ec509b9ea6fb7b7bf4aa99b05c4ec
noelru94 <noelru94@gmail.com>
2019-08-06 16:26:53 -0400

    switch cases that take in a user input via process.argv[2] which will envoke a function attached to the case

--- liri.js
+++ liri.js
@@ -6,3 +6,24 @@ var Spotify = require('node-spotify-api');
 
 // var spotify = new Spotify(Keys.spotify);
 
+var input = process.argv[2];
+
+switch(input){
+
+    case 'concert-this':
+    concertThis();
+    break;
+
+    case 'spotify-this-song':
+    spotifyThisSong();
+    break;
+
+    case 'movie-this':
+    movieThis();
+    break;
+
+    case 'do-what-it-says':
+    doWhatItSays();
+    break;
+
+}
